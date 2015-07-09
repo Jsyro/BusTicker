4d9382d98dddba9e0453981d2f52cfec0646c40a
Jsyro <jasyrotuck@gmail.com>
2015-07-09 16:38:21 -0700

    one mroe thing

--- transdata.py
+++ transdata.py
@@ -1,4 +1,52 @@
-import csv 
+import csv
+import sqlite3 as sql
+import sqlalchemy
+import os
 
-f_agency = csv.reader('./../bin/agency.txt');
-print f_agency
+class transdata:
+
+	def init(self, agency="Victoria"):
+		self.agency = agency
+		self.initdb()
+		
+	def get_next_bus(self, route_id, stop_id):
+		print "the next " + str(route_id) + " leaves in 15 minutes"
+		
+	def initdb(self):
+		conn = sql.connect(os.getcwd() + "\\transdata\\bus_ticker.db")
+		self.cur = conn.cursor()
+		for fname in os.listdir("transdata"):
+			(name,ext) = os.path.splitext(os.getcwd() + "\\transdata\\" + fname)
+			if (ext == ".txt"):
+				os.rename(name+ext, name +".csv")
+
+		
+	def get_routes (self, agency="Victoria"):	
+		self.cur.execute("SELECT * FROM routes where agency_id = '1';")
+		self.routes = self.cur.fetchall();
+	#	for route in self.routes:
+	#		print route[1]
+			
+	def get_route (self, request):
+		if self.routes is None:	
+			print ("INITIALIZE routes... call get_routes()")
+		for route in self.routes:
+			routeid = route[2] 
+			if routeid == request:
+				return route
+		
+	def	get_trip_ids (self, route):
+		
+		tripids= []
+		routetripid ={}
+		self.cur.execute("SELECT * FROM trips where route_id = '" + route[1] + "';")
+		sqlret = self.cur.fetchall();
+		for row in sqlret:
+			.add(row[3])
+		
+		
+td = transdata()
+td.init()
+td.get_routes()		
+r39 = td.get_route("39")
+td.get_trip_ids(r39)
