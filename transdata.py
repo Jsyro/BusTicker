1407005196943fe1406a23ca186ae4826807b57f
Jsyro <Jasyrotuck@gmail.com>
2015-07-16 19:36:21 -0700

    it works

--- transdata.py
+++ transdata.py
@@ -12,14 +12,25 @@ class transdata:
     def init(self, agency="1"):
         self.initdb()
         self.agency = agency
-        self.agencyRoutes = {}
+
+        #dictionary of {route:List<trip_id>
         self.routeTrips = {}
+        
+        #dictionary {route:List<time_strings>
         self.routeTimes = {}
+        
+        # list of routes (2 digits) given by user
         self.userRoutes = []
-        self.userTrips = []
-        self.userStops = []
-        self.calendar_date = "" 
-        self.service_ids = []
+        
+        # list of stopids (6 digit) given by user
+        self.userStops = {}
+        
+        # list of service_id's baesd on the routes and the dates
+        self.lst_ServiceId = []
+        
+        self.lst_RouteId = []
+        
+        self.weekday = -1
 		
     def initdb(self):
         conn = sql.connect(os.getcwd() + "\\transdata\\bus_ticker.db")
@@ -28,8 +39,6 @@ class transdata:
 		#			1) import all the .txt to tables
 		#			2) add indices to (stop_times, routes, and trips)
 	
-                
-                
     """  ------  Routes  -------- """ 
     # takes number and replaces with full route_id
     
@@ -39,14 +48,21 @@ class transdata:
     def print_routes(self):
         print self.userRoutes
 
+    def remove_route (self, routenum):
+        self.userRoutes.remove(routenum)
+        
     """ ------  Stop IDs  -------- """ 
 
     def add_stopID (self,newStopID):
-        self.userStops.append(newStopID)
+        self.userStops[newStopID] = []
           
     def print_stopIDs (self):
         print self.userStops
         
+    def remove_stopID (self, stopID):
+        self.userStops.remove(stopID)
+        
+    """------------BUILDING THE BUS TIMES ------------"""    
         
     def get_stops_for_trip(self, tripID):
         self.cur.execute("SELECT stop_ID, departure_time FROM stop_times where trip_ID ='" + tripID + "';")
@@ -54,53 +70,68 @@ class transdata:
         
     def	get_trip_IDs (self, routeID):
         self.cur.execute("SELECT trip_ID, trip_headsign FROM trips where route_ID = '" + routeID + "';")
-        return self.cur.fetchall();
+        return self.cur.fetchall()
         
         
-    """ ------- Build DATA ----  """     
+    """ ------- Build DATA ----  """  
+    
+    def get_headsigns (self):
+        return
+        
+    
+    def set_date_string(self):    
+        self.date_string = time.strftime("%Y%m%d")
+        
+    def set_weekday(self):    
+        wkday = datetime.datetime.today().weekday()
+        
+        if  wkday == 0:
+            wkday = "monday"
+        elif wkday == 1:
+            wkday = "tuesday"
+        elif  wkday == 2:
+            wkday = "wednesday"
+        elif  wkday == 3:
+            wkday = "thursday"
+        elif  wkday == 4:
+            wkday = "friday"
+        elif  wkday == 5:
+            wkday = "saturday"
+        elif  wkday == 6:
+            wkday = "sunday"
+        else:
+            print "INVALID DAY OF THE WEEK"
+            exit(3)
+        self.weekday = wkday    
+            
+    
     def get_user_routes (self):	
         # takes the self.userRoutes list and REPLACES it with their route_id's
         user_routes_string = ', '.join(self.userRoutes)
         sql = 'SELECT route_id FROM routes NATURAL JOIN agency WHERE route_short_name IN ({})'.format(user_routes_string)
         self.cur.execute(sql)
-        self.userRoutes = [x[0] for x in self.cur.fetchall()]
+        self.lst_RouteId = [x[0] for x in self.cur.fetchall()]
         
     def get_user_trips(self):
-        # take the self.userRoutes list of route_id's and return all of those routes' trips
-        service_string = time.strftime("%Y%m%d")
-        weekday = datetime.datetime.today().weekday()
-        if  weekday == 0:
-            weekday = "monday"
-        if  weekday == 1:
-            weekday = "tuesday"
-        if  weekday == 2:
-            weekday = "wednesday"
-        if  weekday == 3:
-            weekday = "thursday"
-        if  weekday == 4:
-            weekday = "friday"
-        if  weekday == 5:
-            weekday = "saturday"
-        if  weekday == 6:
-            weekday = "sunday"
-            
-           
-        
-        user_routes_string = ["'{}'".format(id) for id in self.userRoutes]
-        user_routes_string = ', '.join(user_routes_string)
-#        print "SELECT service_id FROM calendar NATURAL JOIN trips WHERE start_date <= '{}' AND end_date > '{}' AND route_id IN ({}) AND {}== '1';".format(service_string, service_string, user_routes_string, weekday)
 
-        self.cur.execute("SELECT DISTINCT service_id FROM calendar NATURAL JOIN trips WHERE start_date <= '{}' AND end_date > '{}' AND route_id IN ({}) AND {}== '1';".format(service_string, service_string, user_routes_string, weekday))
-                            
-        self.service_ids = self.cur.fetchone()[0]
-        # TO-DO: Figure out if i ever need to handle more than one service_id
-		
-        for routeid in self.userRoutes:
-            self.cur.execute("SELECT trip_id                \
-                                    FROM trips NATURAL JOIN routes          \
-                                    WHERE route_id == '" + routeid + "'	\
-									AND service_id == '" + self.service_ids + "';")
-										
+        # take the self.userRoutes list of route_id's and return all of those routes' trips
+        user_routes_string = ["'{}'".format(id) for id in self.lst_RouteId]
+        user_routes_string = ', '.join(user_routes_string)
+        
+        #get service_ids based on today's date, the routes selected and day of the week
+        self.cur.execute("SELECT DISTINCT service_id FROM calendar NATURAL JOIN trips WHERE start_date <= '{}' AND end_date > '{}' AND route_id IN ({}) AND {}== '1';".format(self.date_string, self.date_string, user_routes_string, self.weekday))
+        self.lst_ServiceId = [x[0] for x in self.cur.fetchall()]
+        
+        #get exceptions based on the date and it's an addition service
+        self.cur.execute("SELECT service_id FROM calendar_dates WHERE date = '{}' AND exception_type == '1' ".format(self.date_string))
+        self.lst_ServiceId += ([x[0] for x in self.cur.fetchall()])
+        
+        #TO-DO: handle calendar_dates table where there are special removals
+        service_string = ["'{}'".format(id) for id in self.lst_ServiceId]
+        service_string = ', '.join(service_string)
+        # TO-DO: (DONE) Figure out if i ever need to handle more than one service_id		
+        for routeid in self.lst_RouteId:
+            self.cur.execute("SELECT trip_id FROM trips NATURAL JOIN routes WHERE route_id == '{}' AND service_id IN ({});".format(routeid, service_string))
             self.routeTrips[routeid] = [x[0] for x in self.cur.fetchall()]
         
     def get_times_for_trips_at_stops(self):
@@ -110,23 +141,48 @@ class transdata:
             
             user_stopid_string = ', '.join(self.userStops)
           
-            sql = 'SELECT departure_time FROM stop_times WHERE trip_id IN ({}) AND stop_id IN ({})'.format(user_trips_string,user_stopid_string)
+            sql = 'SELECT departure_time, stop_id, trip_headsign FROM stop_times NATURAL JOIN trips WHERE trip_id IN ({}) AND stop_id IN ({})'.format(user_trips_string,user_stopid_string)
             self.cur.execute(sql)
-            self.routeTimes[route] = [x for x in self.cur.fetchall()]
+            self.routeTimes[route] = self.cur.fetchall()
             self.routeTimes[route].sort()
         
         
     def build_data(self, routenum):
+        self.set_weekday()
+        self.set_date_string()
         self.get_user_routes()
         self.get_user_trips()
         self.get_times_for_trips_at_stops()
-        print self.routeTimes
+        #print self.routeTimes
         return 
 
+    """ ----- QUERIES ------ """ 
+    def q_next_bus (self, routeNum):
+        routeId = ''
+        curTime = time.strftime("%H:%M:%S")
+        
+        for id in self.lst_RouteId:
+            if id[:len(routeNum)] == routeNum:
+                routeId = id
 
+        if routeId is None:
+            print "route not included in the build, add_route(routeNumber), build_data(): then try again"
+            
+        for stop_time in self.routeTimes[routeId]:
+            if stop_time[:2] == "24":
+                stop_time[:2] == "00"
+            if stop_time[:2] == "25":
+                stop_time[:2] == "01"
+            if stop_time[0] > curTime:
+                return "The next {} {} arrives at {} (#{})".format(routeNum,stop_time[2],stop_time[0], stop_time[1])
+            
 td = transdata()
-td.init()
+td.init("1") # 1 is agency ID of Victoria
 td.add_route("26")
 td.add_route("39")
 td.add_stopID("100919")
+td.add_stopID("101424")
 td.build_data("26")
+
+print td.q_next_bus("26")
+print td.q_next_bus("39")
